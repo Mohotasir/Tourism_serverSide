c07e0627b18ba2a3056aff419e058204491575e4
mohotasir <talukdertahsin643@gmail.com>
2024-04-28 22:32:48 +0600

    added update method

--- index.js
+++ index.js
@@ -39,11 +39,51 @@ async function run() {
           const spot = await databaseCollection.findOne(query);
           res.send(spot);
     })
+    app.get('/spots', async (req, res) => {
+        const email = req.query.email; 
+        const query = email ? { email: email } : {}; 
+        const cursor = databaseCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+    
+
     app.post('/spots',async(req,res)=>{
         const spot = req.body;
-         //console.log("new user",user)
          const result = await databaseCollection.insertOne(spot);
          res.send(result);
+    })
+    app.put("/spots/:id",async(req,res)=>{
+        const id  = req.params.id;
+        const spot = req.body;
+        console.log(id,spot)
+        const filter = {_id : new ObjectId(id)}
+        const options = {upsert: true}
+        const updateData = {
+            $set: {
+                img: spot.img,
+                spotName: spot.spotName,
+                country_name: spot.country_name,
+                location: spot.location,
+                sd:spot.sd,
+                ac: spot.ac,
+                seasonality: spot.seasonality,
+                tt: spot.tt,
+                vr: spot.vr,
+                
+                
+            }
+        };
+        
+        const result = await databaseCollection.updateOne(filter,updateData,options);
+        res.send(result);
+    })
+    app.delete('/spots/:id',async(req,res)=>{
+           const id = req.params.id;
+           console.log("deleted id is:",id)
+           const query = {_id : new ObjectId(id)};
+           const result = await databaseCollection.deleteOne(query);
+           res.send(result)
     })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
